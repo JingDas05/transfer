32ae27bbc736958de144bd0eb8f6be11b40d03c6
wusi05 <wusi05@126.com>
2017-07-11 07:13:34 +0800

    初始化es程序

--- src/main/java/com/sfalw/ws/ReadAndCreat.java
+++ src/main/java/com/sfalw/ws/ReadAndCreat.java
@@ -5,17 +5,25 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.joda.time.DateTime;
+//import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.joda.time.DateTime;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Date;
 
 /**
@@ -26,30 +34,41 @@ import java.util.Date;
  */
 public class ReadAndCreat {
 
-    private static String es_host = "172.18.12.125";
+    //    private static String es_host = "172.18.12.125";
+    private static String es_host = "localhost";
     private static int es_port = 9300;
-    public static String index = "writ_stick";
-    public static String type = "fycase";
+    //    public static String index = "writ_stick";
+    public static String index = "diary";
+    //    public static String type = "fycase";
+    public static String type = "page";
     public static Client client;
 
     static {
-        client = new TransportClient()
-                .addTransportAddress(new InetSocketTransportAddress(es_host, es_port));
+        Settings settings = Settings.settingsBuilder()
+                .put("cluster.name", "ask.dog").build();
+        try {
+            client = TransportClient.builder().settings(settings).build()
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_host), es_port));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+//        client = new TransportClient()
+//                .addTransportAddress(new InetSocketTransportAddress(es_host, es_port));
     }
 
     public static void main(String[] args) {
-
+        readAndCreate();
     }
 
-    private void readAndCreate() {
+    private static void readAndCreate() {
         // 从es批量查询
-        RangeQueryBuilder queryBuilder = new RangeQueryBuilder("_timestamp")
-                .from(new DateTime("2017-05-01T00:00:00").getMillis())
+        RangeQueryBuilder queryBuilder = new RangeQueryBuilder("createTime")
+                .from(new DateTime("2015-05-01T00:00:00").getMillis())
                 .to(new Date().getTime());
         SearchRequestBuilder searchRequestBuilder = client.prepareSearch(index)
                 .setTypes(type)
                 .setQuery(queryBuilder)
-                .setSize(20)
+                .setSize(10)
                 .setScroll(new TimeValue(60 * 1000))
                 .setSearchType(SearchType.QUERY_THEN_FETCH);
         SearchResponse searchResponse = searchRequestBuilder.execute()
@@ -63,15 +82,35 @@ public class ReadAndCreat {
             return;
         }
         // 写入文件，文件名为id.json
-        for (SearchHit searchHit: firstSearchHits) {
+        for (SearchHit searchHit : firstSearchHits) {
             try {
-                FileOutputStream fileOutputStream = new FileOutputStream(searchHit.getId()+".json");
-                FileChannel fileChannel = fileOutputStream.getChannel();
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-
-            } catch (FileNotFoundException e) {
+                Files.write(Paths.get(searchHit.getId() + ".json"),
+                        searchHit.source(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+            } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        firstSearchHits = null;
+        //接着写剩下的
+        long alreadyStore = 0;
+        alreadyStore = alreadyStore + currentResultNum;
+        SearchHits searchHits;
+        while (alreadyStore < total) {
+            SearchResponse response = client.prepareSearchScroll(scrollId)
+                    .setScroll(new TimeValue(60 * 1000))
+                    .execute().actionGet();
+            searchHits = response.getHits();
+            scrollId = response.getScrollId();
+            // 开始写入文件
+            for (SearchHit searchHit : searchHits) {
+                try {
+                    Files.write(Paths.get(searchHit.getId() + ".json"),
+                            searchHit.source(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            alreadyStore = alreadyStore + searchHits.getHits().length;
         }
     }
 }
--- src/main/java/com/sfalw/ws/ReadAndInsert.java
+++ src/main/java/com/sfalw/ws/ReadAndInsert.java
@@ -1,6 +1,16 @@
 package com.sfalw.ws;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 
 /**
  * 循环读取每个文件，积累到100，批量更新es
@@ -10,15 +20,38 @@ import org.elasticsearch.action.bulk.BulkRequestBuilder;
  */
 public class ReadAndInsert {
 
+    public static void main(String[] args) {
+
+    }
+
     public static void bulkUpdate() {
+        byte[] jsonData = new byte[0];
         BulkRequestBuilder bulkRequestBuilder = ReadAndCreat.client.prepareBulk();
         // 依次读取json, 累计100
-
-        for (;;){
-            bulkRequestBuilder.add(ReadAndCreat.client
-                    .prepareIndex(ReadAndCreat.index, ReadAndCreat.type)
-                    .setSource(json));
+        File parentFile = new File("../data");
+        RegexFileFilter filter = new RegexFileFilter("^(.*?)");
+        Iterator<File> fileIterator = FileUtils.iterateFiles(parentFile, filter, DirectoryFileFilter.DIRECTORY);
+        int bound = 150;
+        int currentBound = 0;
+        boolean isSuccess = true;
+        while (fileIterator.hasNext() && isSuccess) {
+            try {
+                jsonData = Files.readAllBytes(Paths.get(fileIterator.next().getAbsolutePath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (currentBound++ <= bound) {
+                bulkRequestBuilder.add(ReadAndCreat.client
+                        .prepareIndex(ReadAndCreat.index, ReadAndCreat.type)
+                        .setSource(jsonData));
+            } else {
+                BulkResponse bulkResponse = bulkRequestBuilder.execute().actionGet();
+                // 复位
+                isSuccess = !bulkResponse.hasFailures();
+                currentBound = 0;
+                bulkRequestBuilder = ReadAndCreat.client.prepareBulk();
+            }
         }
-        bulkRequestBuilder.execute().actionGet();
     }
 }
+
