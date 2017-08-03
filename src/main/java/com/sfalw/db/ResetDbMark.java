5d218ec319450e5f61bbae8541a1d1282ef78477
wusi06 <wusi0109@163.com>
2017-08-03 13:14:10 +0800

    简单的jdbctemplate 置位查询

---
+++ src/main/java/com/sfalw/db/ResetDbMark.java
@@ -0,0 +1,44 @@
+package com.sfalw.db;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.util.List;
+
+/**
+ * @author wusi
+ * @version 2017/8/2 17:24
+ */
+public class ResetDbMark {
+
+    private static DriverManagerDataSource dataSource;
+    private static JdbcTemplate jdbcTemplate;
+
+    static {
+        dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://172.18.12.118:5432/sfal2");
+        dataSource.setUsername("thunisoft");
+        dataSource.setPassword("123456");
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public static void main(String[] args) {
+        // 查询所有模式名
+        List<String> schamas = jdbcTemplate.queryForList("SELECT DISTINCT schemaname FROM pg_tables WHERE schemaname NOT LIKE 'pg%' AND schemaname NOT LIKE 'sql_%' AND \n" +
+                "schemaname NOT LIKE 'information%';", String.class);
+        for (String s : schamas) {
+            List<String> tableNames = jdbcTemplate.queryForList("select tablename from pg_tables where schemaname= ? ;",
+                    String.class, s);
+            // 查询每个模式下面的所有表
+            for (String t : tableNames) {
+//                System.out.print("update " + s + "." + t + " set n_mark = 0;");
+                try {
+                    jdbcTemplate.execute("update " + s + "." + t + " set n_mark = 0;");
+                }catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
